b87c62cd0b6613829332c31e9e1b2510dbfd449b
DankAvHi <thexcat86@gmail.com>
2023-01-14 15:03:54 +0300

    [task] complete player movement script

--- Assets/Entities/Player/PlayerMovement.cs
+++ Assets/Entities/Player/PlayerMovement.cs
@@ -4,35 +4,52 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D playerRigidbody2D;
+    [SerializeField] private Collider2D playerColider2D;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private LayerMask layerMask;
+    private Vector2 boxSize;
+    private float maxJumpAvailableDistance = 0.1f;
 
-    [SerializeField]
-    private float speed = 10.0f;
-    [SerializeField]
-    private float jumpForce = 10.0f;
-    private Rigidbody2D playerRigidbody;
-    private bool isGrounded;
 
     void Start()
     {
-        this.playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        if (playerRigidbody2D == null && playerColider2D == null)
+        {
+            playerRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+            playerColider2D = gameObject.GetComponent<Collider2D>();
+            if (playerRigidbody2D == null && playerColider2D == null)
+            {
+                throw new System.Exception("Some components is not added to gameObject and PlayerMovement script");
+            }
+        }
+        boxSize = playerColider2D.bounds.size;
+
     }
 
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space) && IsGrounded()){
-            playerRigidbody.velocity = new Vector2(0, jumpForce * speed);
-        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded()) jump();
 
-        float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        movement();
 
-        playerRigidbody.velocity = new Vector2(horizontalMovement * speed, playerRigidbody.velocity.y) * Time.fixedDeltaTime;
     }
 
-    private bool IsGrounded()
+    private void movement()
     {
-        var groundCheck = Physics2D.Raycast(transform.position, -Vector2.up, gameObject.GetComponent<CapsuleCollider2D>().bounds.extents.y +0.1f);
-       
-        return groundCheck.collider != null && groundCheck.collider.CompareTag("Ground");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        playerRigidbody2D.velocity = new Vector2(horizontalInput * speed, playerRigidbody2D.velocity.y);
+
     }
+
+    private void jump()
+    {
+        playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, jumpForce);
+    }
+
+
+    private bool isGrounded() => Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, maxJumpAvailableDistance, layerMask);
+
 }
